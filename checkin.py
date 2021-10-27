e80b938a0acb01f9f2f2ea476532b6e5b960bcc5
Alan <72089029+L-ESheng@users.noreply.github.com>
2021-10-28 07:37:01 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -92,9 +92,9 @@ for x in object['recommend']:
         buffer.append(data2)
         count+=1
         #time.sleep(1)
-        if count>=600:
+        if count>=360:
             break
-    if count>=600:
+    if count>=360:
         break
 url = "http://music.163.com/weapi/feedback/weblog"
 postdata={
